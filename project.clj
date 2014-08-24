9965aec066ee83f867a484879d583adfc6faeeea
Daniel James <dwhjames@gmail.com>
2014-08-24 17:07:03 -0400

    update Datomic version

--- project.clj
+++ project.clj
@@ -3,5 +3,6 @@
   :url "https://github.com/pellucidanalytics/datomic-linklist"
   :license {:name "Apache License, Version 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [com.datomic/datomic-free "0.9.4755"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [com.datomic/datomic-free "0.9.4880.6"
+                  :exclusions [org.clojure/clojure]]])
